2344c464bb4243735a13470857e9679328d12e02
Daniel Ran <danielr1735@gmail.com>
2018-06-22 19:45:45 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -35,22 +35,10 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do | style, languagesHash |
     languagesHash.each do | languageHash, dataHash |
-
-
-
       new_hash[languageHash] ||= dataHash
       new_hash[languageHash][:style] ||= []
       new_hash[languageHash][:style].push(style)
-
-      # if !new_hash[languageHash]
-      #   dataHash[:style].push(style)
-      #   new_hash[languageHash] = dataHash
-      #
-      # else
-      #   new_hash[languageHash][:style].push(style)
-
     end
   end
-  puts new_hash
   new_hash
 end
